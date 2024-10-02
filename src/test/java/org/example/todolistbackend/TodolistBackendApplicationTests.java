8f60a026d9d38b9e8a444bb8a546cc8551caa507
Mihai Rusu <rusumiihai@gmail.com>
2024-10-02 10:40:37 +0300

    created project

---
+++ src/test/java/org/example/todolistbackend/TodolistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todolistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
